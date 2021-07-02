110f1c4d2aba1f9a4c2c817e5cc5959743f00d8d
Trina <trinamceary@gmail.com>
2021-07-02 13:08:30 -0700

    all data displayed in browser

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const stats = {
 round: 0,
-player: 0,
-computer: 0,
+playerScore: 0,
+computerScore: 0,
+playerChoice: '',
+computerChoice: '',
+tieCount: 0,
+winner: ''
 }
 
 
@@ -16,39 +20,47 @@ function chooseRandom() {
 function playRound(playerSelection, computerSelection) {
   const choices = ['rock', 'paper', 'scissors'];
 
+  stats.playerChoice = playerSelection;
+  stats.computerChoice = computerSelection;
+
    if (playerSelection === computerSelection) {
+     stats.tieCount++;
+    stats.winner = 'Tie';
     console.log(`${playerSelection} vs. ${computerSelection}...It\'s a tie!`);
   }
     if (playerSelection === choices[0] && computerSelection == choices[2]) {
-    stats.player++; 
+    stats.playerScore++; 
+    stats.winner = 'You Win';
     console.log(`${playerSelection} vs. ${computerSelection}... Rock beats scissors! You win!`)
   }
     if (playerSelection === choices[1] && computerSelection == choices[0]) {
-    stats.player++;
+    stats.playerScore++;
+    stats.winner = 'You Win';
     console.log(`${playerSelection} vs. ${computerSelection}... Paper covers rock! You win!`)
   }
    if (playerSelection === choices[2] && computerSelection == choices[1]) {
-    stats.player++;
+    stats.playerScore++;
+    stats.winner = 'You Win';
     console.log(`${playerSelection} vs. ${computerSelection}... Scissors cut paper! You win!`)
   } 
    if (playerSelection === choices[0] && computerSelection == choices[1]) {
-    stats.computer++ 
+    stats.computerScore++ 
+    stats.winner = 'Computer Wins'
     console.log(`${playerSelection} vs. ${computerSelection}... Paper covers rock! You lose!`)
   }
     if (playerSelection === choices[1] && computerSelection == choices[2]) {
-    stats.computer++
+    stats.computerScore++;
+    stats.winner = 'Computer Wins';
     console.log(`${playerSelection} vs. ${computerSelection}... Scissors cut paper! You lose!`)  
   }
     if (playerSelection === choices[2] && computerSelection == choices[0]) {
-    stats.computer++
+    stats.computerScore++;
+    stats.winner = 'Computer Wins';
     console.log(`${playerSelection} vs. ${computerSelection}...Rock beats scissors! You lose!`)
   }
   return;
 }
 
-function render() {
-
-}
 
 //**************EVENT HANDLERS**********/
 function handleWeaponClick() {
@@ -57,23 +69,26 @@ function handleWeaponClick() {
     b.addEventListener('click', () => {
       playRound(b.getAttribute("id"), chooseRandom());
       stats.round++;
+      stats.playerChoice = b.getAttribute("id");
       displayStats()
       console.log(stats.round)
     })
   });
 }
 
-  //why does button dissapear when other button is clicked
 function handleResetClick() {
   const resetBtn = document.querySelector("#reset");
 
   resetBtn.addEventListener('click', () => {
     stats.round = 0;
-    stats.player = 0;
-    stats.computer = 0;
+    stats.playerScore = 0;
+    stats.computerScore = 0;
+    stats.tieCount = 0
+    stats.playerChoice = '';
+    stats.computerChoice = '';
+    stats.winner = '';
     
     displayStats();
-    console.log("done. Round" + stats.round)
   })
 }
 
@@ -85,9 +100,17 @@ function displayStats() {
   statbox.innerHTML = 
   `<h2>Round: ${stats.round}</h2>
   <div id="scores"> 
-   Player: ${stats.player}
-   Computer: ${stats.computer}
-  </div>`
+   Player: ${stats.playerScore}
+   Computer: ${stats.computerScore}
+   Tie: ${stats.tieCount}
+  </div>
+  <div id="game-results"> 
+   Player Chose: ${stats.playerChoice}
+   Computer Chose: ${stats.computerChoice}
+   Winner: ${stats.winner}
+  </div>
+  `
+  
 }
 
 
